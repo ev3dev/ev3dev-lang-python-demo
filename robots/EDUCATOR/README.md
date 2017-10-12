70988d7528900e9edf0f71e2df37f49f4211288d
Greg Cowell <greg.cowell@gmail.com>
2017-10-12 10:08:19 +1000

    Create example scripts for the EDUCATOR robot (#13)

---
+++ robots/EDUCATOR/README.md
@@ -0,0 +1,8 @@
+# EDUCATOR
+These demonstration scripts are for the EDUCATOR robot that can be built using
+the Education kit (45544). You can also build a close approximation using the
+Home kit (31313) if you purchase some additional parts such as a gyro sensor and
+ultrasonic sensor. You can substitute the ball and socket from the Education
+kit with a single rear wheel.
+
+**Building Instructions**: https://education.lego.com/en-au/support/mindstorms-ev3/building-instructions
---
+++ robots/EDUCATOR/ultrasonic.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Use robot to reposition cuboid.
+
+This script is a simple demonstration of the ultrasonic sensor and medium
+motor.
+"""
+
+from ev3dev.motor import MoveSteering, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C
+from ev3dev.sensor.lego import UltrasonicSensor
+from time import sleep
+
+motor_pair = MoveSteering(OUTPUT_B, OUTPUT_C)
+medium_motor = MediumMotor(OUTPUT_A)
+ultrasonic_sensor = UltrasonicSensor()
+
+# Start robot moving forward
+motor_pair.on(steering=0, speed_pct=10)
+
+# Wait until robot less than 3.5cm from cuboid
+while ultrasonic_sensor.distance_centimeters > 3.5:
+    sleep(0.01)
+
+# Stop moving forward
+motor_pair.off()
+
+# Lower robot arm over cuboid
+medium_motor.on_for_degrees(speed_pct=-10, degrees=90)
+
+# Drag cuboid backwards for 2 seconds
+motor_pair.on_for_seconds(steering=0, speed_pct=-20, seconds=2)
+
+# Raise robot arm
+medium_motor.on_for_degrees(speed_pct=10, degrees=90)
+
+# Move robot away from cuboid
+motor_pair.on_for_seconds(steering=0, speed_pct=-20, seconds=2)
