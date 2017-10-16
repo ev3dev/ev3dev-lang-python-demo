c5e11f2df2490e4f316b45327ec95cc96fbfac21
Daniel Walton <dwalton76@gmail.com>
2017-10-15 23:53:13 -0400

    GRIPP3R init commit

---
+++ robots/GRIPP3R/GRIPP3R.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+
+import logging
+import signal
+import sys
+from ev3dev.motor import OUTPUT_A, OUTPUT_B, OUTPUT_C, MediumMotor
+from ev3dev.control.rc_tank import RemoteControlledTank
+from ev3dev.sensor.lego import TouchSensor
+from threading import Thread, Event
+
+log = logging.getLogger(__name__)
+
+
+class MonitorTouchSensor(Thread):
+    """
+    Monitor Gripper's TouchSensor and close the gripper when
+    the TouchSensor is pressed
+    """
+
+    def __init__(self, parent):
+        Thread.__init__(self)
+        self.parent = parent
+        self.shutdown_event = Event()
+        self.monitor_ts = Event()
+
+    def __str__(self):
+        return "MonitorTouchSensor"
+
+    def run(self):
+
+        while True:
+
+            if self.parent.ts.is_released:
+                if self.parent.ts.wait_for_pressed(timeout_ms=1000):
+                    self.parent.claw_close(True)
+
+            if self.shutdown_event.is_set():
+                log.info('%s: shutdown_event is set' % self)
+                break
+
+
+class Gripper(RemoteControlledTank):
+    """
+    To enable the medium motor toggle the beacon button on the EV3 remote.
+    """
+    CLAW_DEGREES_OPEN = 225
+    CLAW_DEGREES_CLOSE = 920
+    CLAW_SPEED_PCT = 50
+
+    def __init__(self, left_motor_port=OUTPUT_B, right_motor_port=OUTPUT_C, medium_motor_port=OUTPUT_A):
+        RemoteControlledTank.__init__(self, left_motor_port, right_motor_port)
+        self.set_polarity(MediumMotor.POLARITY_NORMAL)
+        self.medium_motor = MediumMotor(medium_motor_port)
+        self.ts = TouchSensor()
+        self.mts = MonitorTouchSensor(self)
+
+        self.shutdown_event = Event()
+        signal.signal(signal.SIGTERM, self.signal_term_handler)
+        signal.signal(signal.SIGINT, self.signal_int_handler)
+
+        self.claw_open(True)
+        self.remote.on_channel4_top_left = self.claw_close
+        self.remote.on_channel4_bottom_left = self.claw_open
+        self.mts.start()
+
+    def shutdown_robot(self):
+
+        if self.shutdown_event.is_set():
+            return
+
+        self.shutdown_event.set()
+        log.info('shutting down')
+        self.mts.shutdown_event.set()
+        self.remote.on_channel4_top_left = None
+        self.remote.on_channel4_bottom_left = None
+        self.left_motor.off(brake=False)
+        self.right_motor.off(brake=False)
+        self.medium_motor.off(brake=False)
+        self.mts.join()
+
+    def signal_term_handler(self, signal, frame):
+        log.info('Caught SIGTERM')
+        self.shutdown_robot()
+
+    def signal_int_handler(self, signal, frame):
+        log.info('Caught SIGINT')
+        self.shutdown_robot()
+
+    def claw_open(self, state):
+        if state:
+            self.mts.monitor_ts.clear()
+            self.medium_motor.on(speed_pct=self.CLAW_SPEED_PCT * -1, block=True)
+            self.medium_motor.off()
+            self.medium_motor.reset()
+            self.medium_motor.on_to_position(speed_pct=self.CLAW_SPEED_PCT,
+                                             position=self.CLAW_DEGREES_OPEN,
+                                             brake=False, block=True)
+            self.mts.monitor_ts.set()
+
+    def claw_close(self, state):
+        if state:
+            self.medium_motor.on_to_position(speed_pct=self.CLAW_SPEED_PCT,
+                                             position=self.CLAW_DEGREES_CLOSE)
+
+
+if __name__ == '__main__':
+
+    # Change level to logging.INFO to make less chatty
+    logging.basicConfig(level=logging.DEBUG,
+                        format="%(asctime)s %(levelname)5s %(filename)s: %(message)s")
+    log = logging.getLogger(__name__)
+
+    # Color the errors and warnings in red
+    logging.addLevelName(logging.ERROR, "\033[91m  %s\033[0m" % logging.getLevelName(logging.ERROR))
+    logging.addLevelName(logging.WARNING, "\033[91m%s\033[0m" % logging.getLevelName(logging.WARNING))
+
+    log.info("Starting GRIPP3R")
+    gripper = Gripper()
+    gripper.main()
+    log.info("Exiting GRIPP3R")
